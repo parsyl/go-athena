7bfc704c64f3803994cd0d3f7f325a1af2558a4a
Andy Trimble <andy.trimble@gmail.com>
2018-09-06 12:46:42 -0600

    Explicitly setting MaxResults

--- rows.go
+++ rows.go
@@ -90,6 +90,7 @@ func (r *rows) Next(dest []driver.Value) error {
 func (r *rows) fetchNextPage(token *string) (bool, error) {
 	var err error
 	r.out, err = r.athena.GetQueryResults(&athena.GetQueryResultsInput{
+		MaxResults:       aws.Int64(1000),
 		QueryExecutionId: aws.String(r.queryID),
 		NextToken:        token,
 	})
